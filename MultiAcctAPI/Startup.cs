a92f97b3b7a7072204608d413fb3f285d957c0f2
Ademir <ademir.belini@scalait.com>
2025-02-01 13:49:19 -0300

    Initial commit

---
+++ MultiAcctAPI/Startup.cs
@@ -0,0 +1,38 @@
+using Microsoft.OpenApi.Models;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "MultiAcctAPI", Version = "v1" });
+        });
+        
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "MultiAcctAPI v1");
+            c.RoutePrefix = string.Empty;
+        });
+    }
+}
